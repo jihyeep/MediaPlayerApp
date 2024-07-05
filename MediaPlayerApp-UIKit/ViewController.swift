deb7271c97814b1a2c3992bf0fcdb132e6961400
Jihye Park <deviloper.park@gmail.com>
2024-07-05 17:50:13 +0900

    Initial Commit

---
+++ MediaPlayerApp-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaPlayerApp-UIKit
+//
+//  Created by 박지혜 on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
