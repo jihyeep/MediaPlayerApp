6f3cde4d3bb00db92d7575c2bf2575919369caf6
Jihye Park <deviloper.park@gmail.com>
2024-07-08 18:01:26 +0900

    feat: AVPlayer 추가 / fix: 스토리보드 삭제 및 SceneDelegate 설정

--- MediaPlayerApp-UIKit/MediaItem.swift
+++ MediaPlayerApp-UIKit/MediaItem.swift
@@ -15,6 +15,7 @@ struct MediaItem {
 
 extension MediaItem {
     static let samples = [
+        MediaItem(url: Bundle.main.url(forResource: "Small World", withExtension: "mp3")!, title: "Short MP3 Sample", isVideo: false),
         MediaItem(url: URL(string:"https://samplelib.com/lib/preview/mp3/sample-3s.mp3")!, title: "Short MP3 Sample", isVideo: false),
         MediaItem(url: URL(string:"https://filesamples.com/samples/audio/mp3/sample2.mp3")!, title: "Long MP3 Sample", isVideo: false),
         MediaItem(url: URL(string:"https://filesamples.com/samples/video/mp4/sample_640x360.mp4")!, title: "Medium MP4 Sample", isVideo: true),
--- MediaPlayerApp-UIKit/MediaPlayerViewController.swift
+++ MediaPlayerApp-UIKit/MediaPlayerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 extension UIButton {
     static func createPlayControlButton(sysytemImage: String, pointSize: CGFloat) -> UIButton {
@@ -21,56 +22,59 @@ extension UIButton {
 
 class MediaPlayerViewController: UIViewController {
     let item: MediaItem
-
+    
+    lazy var playerItem = AVPlayerItem(url: item.url)
+    lazy var player = AVPlayer(playerItem: playerItem)
+    
     var goPrev: (() -> Void)?
     var goNext: (() -> Void)?
-
+    
     var isPlaying: Bool = false {
         didSet {
             playButton.isHidden = isPlaying
             pauseButton.isHidden = !isPlaying
         }
     }
-
+    
     private let imageView: UIImageView = {
         let view = UIImageView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-
+    
     let playButton = UIButton.createPlayControlButton(sysytemImage: "play.fill", pointSize: 48)
     let pauseButton = UIButton.createPlayControlButton(sysytemImage: "pause.fill", pointSize: 48)
     let prevButton = UIButton.createPlayControlButton(sysytemImage: "backward.fill", pointSize: 32)
     let nextButton = UIButton.createPlayControlButton(sysytemImage: "forward.fill", pointSize: 32)
-
+    
     lazy var playControlStack: UIStackView = {
         let view = UIStackView(arrangedSubviews: [prevButton, playButton, pauseButton, nextButton])
         view.distribution = .equalCentering
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-
+    
     private let progressBar: UIProgressView = {
         let view = UIProgressView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-
+    
     private let volumeSlider: UISlider = {
         let view = UISlider()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-
+    
     init(item: MediaItem) {
         self.item = item
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -79,71 +83,106 @@ class MediaPlayerViewController: UIViewController {
         setupUI()
         preparePlay()
     }
-
+    
     private func setupUI() {
         view.addSubview(imageView)
         imageView.image = UIImage(systemName: item.isVideo ? "video.square.fill" : "music.note")
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .systemGray5
-
+        
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150),
             imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
         ])
-
+        
         view.addSubview(progressBar)
-
-        progressBar.progress = 0.5
-
+        
         NSLayoutConstraint.activate([
             progressBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             progressBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             progressBar.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 40),
             progressBar.heightAnchor.constraint(equalToConstant: 8)
         ])
-
+        
         view.addSubview(playControlStack)
-
+        
         NSLayoutConstraint.activate([
             playControlStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             playControlStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             playControlStack.topAnchor.constraint(equalTo: progressBar.bottomAnchor, constant: 30),
         ])
-
+        
         playButton.addAction(UIAction { [weak self] _ in
             self?.play()
             self?.isPlaying = true
         }, for: .touchUpInside)
-
+        
         pauseButton.addAction(UIAction { [weak self] _ in
             self?.pause()
             self?.isPlaying = false
         }, for: .touchUpInside)
-
+        
         prevButton.addAction(UIAction { [weak self] _ in
             self?.goPrev?()
         }, for: .touchUpInside)
-
+        
         nextButton.addAction(UIAction { [weak self] _ in
             self?.goNext?()
         }, for: .touchUpInside)
-
+        
         prevButton.isEnabled = goPrev != nil /// goPrev이 nil이 아니면 prevButton 활성화
         nextButton.isEnabled = goNext != nil
+        
+        // 드래그(pan gesture)
+        progressBar.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(seekMusic)))
     }
-
+    
+    // 주기적으로 현재 재생 시간과 총 재생 시간을 확인하여 progress bar를 업데이트
     func preparePlay() {
         isPlaying = false
+        
+        // 총 재생 시간
+        print(playerItem.duration)
+        
+        // 0.1초마다 주기적으로 timeObserver 호출
+        let interval = CMTimeMakeWithSeconds(0.1, preferredTimescale: Int32(NSEC_PER_SEC))
+        // TimeObserver 추가
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main, using: { [weak self] elapsedSeconds in
+            if let currentItem = self?.player.currentItem {
+                // 총 재생 시간
+                let duration = CMTimeGetSeconds(currentItem.duration)
+                // 현재 재생 시간
+                let currentTime = CMTimeGetSeconds(currentItem.currentTime())
+                
+                // UI 업데이트이므로 메인 큐에서 실행
+                DispatchQueue.main.async {
+                    if duration.isNaN || duration == 0 {
+                        self?.progressBar.progress = 0
+                    } else {
+                        self?.progressBar.progress = Float(currentTime / duration)
+                    }
+                }
+                
+                print("Duration: \(duration) s")
+                print("Current time: \(currentTime) s")
+            }
+        })
     }
-
+    
     func play() {
         print("play")
+        player.play()
     }
-
+    
     func pause() {
         print("pause")
+        player.pause()
+    }
+
+    @objc func seekMusic(_ sender: UIProgressView) {
+        print(sender)
     }
 }
 
