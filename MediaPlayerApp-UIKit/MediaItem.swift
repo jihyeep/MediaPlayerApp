e59d6f32895272c6d7cd50778acdf2857220879d
Jihye Park <deviloper.park@gmail.com>
2024-07-09 10:37:17 +0900

    fix: 미디어파일 로딩 오류 해결

--- MediaPlayerApp-UIKit/MediaItem.swift
+++ MediaPlayerApp-UIKit/MediaItem.swift
@@ -15,10 +15,15 @@ struct MediaItem {
 
 extension MediaItem {
     static let samples = [
-        MediaItem(url: Bundle.main.url(forResource: "Small World", withExtension: "mp3")!, title: "Short MP3 Sample", isVideo: false),
-        MediaItem(url: URL(string:"https://samplelib.com/lib/preview/mp3/sample-3s.mp3")!, title: "Short MP3 Sample", isVideo: false),
-        MediaItem(url: URL(string:"https://filesamples.com/samples/audio/mp3/sample2.mp3")!, title: "Long MP3 Sample", isVideo: false),
-        MediaItem(url: URL(string:"https://filesamples.com/samples/video/mp4/sample_640x360.mp4")!, title: "Medium MP4 Sample", isVideo: true),
-        MediaItem(url: URL(string:"https://filesamples.com/samples/video/mp4/sample_960x540.mp4")!, title: "Large MP4 Sample", isVideo: true),
+        MediaItem(url: Bundle.main.url(forResource: "AudioSample1", withExtension: "mp3")!, title: "Small World", isVideo: false),
+        MediaItem(url: Bundle.main.url(forResource: "AudioSample2", withExtension: "mp3")!, title: "sample", isVideo: false),
+        MediaItem(url: Bundle.main.url(forResource: "AudioSample3", withExtension: "mp3")!, title: "sample2", isVideo: false),
+        MediaItem(url: Bundle.main.url(forResource: "VideoSample1", withExtension: "mp4")!, title: "Bigbang - Monster", isVideo: true),
+        MediaItem(url: Bundle.main.url(forResource: "VideoSample2", withExtension: "mov")!, title: "Samsung Premiere 2013 Opening", isVideo: true),
+        MediaItem(url: Bundle.main.url(forResource: "VideoSample3", withExtension: "mov")!, title: "SaturnV", isVideo: true)
+        
+//        MediaItem(url: URL(string:"https://samplelib.com/lib/preview/mp3/sample-3s.mp3")!, title: "Short MP3 Sample", isVideo: false),
+//        MediaItem(url: URL(string:"https://filesamples.com/samples/video/mp4/sample_640x360.mp4")!, title: "Medium MP4 Sample", isVideo: true),
+//        MediaItem(url: URL(string:"https://filesamples.com/samples/video/mp4/sample_960x540.mp4")!, title: "Large MP4 Sample", isVideo: true),
     ]
 }
